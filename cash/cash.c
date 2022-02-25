78bd3f0713dc1da5e69df4fce8ddc2f558cd5a94
Javier Cristobal <jctyasociados@gmail.com>
2022-02-25 22:55:58 +0100

    cash

--- cash/cash.c
+++ cash/cash.c
@@ -2,13 +2,16 @@
 #include <math.h>
 #include <cs50.h>
 
+// Function to get the change owed in a float value.
 float getValue(void);
+// Function that counts the cents coins as an integer.
 int coins(int cents);
 
 int main(void)
 {
     float owed = getValue();
     int cents = round(owed * 100);
+    // print number of cent coins returned
     printf("Coins returned: %d\n", coins(cents));
 }
 
@@ -20,7 +23,7 @@ float getValue(void)
         change = get_float("Change owed: ");
     }
     while (change < 0.00);
-return change;
+    return change;
 }
 
 int coins(int cents)
@@ -29,24 +32,24 @@ int coins(int cents)
     
     while (cents > 0)
     {
-        if (cents >= 25)
+        if (cents >= 25) // check number of quarters to cents.
         {
             cents -= 25;
             count++;
         }
-        else if (cents >= 10)
+        else if (cents >= 10) // check number of dimes to cents.
         {
             cents -= 10;
             count++;
         }
-        else if (cents >= 5)
+        else if (cents >= 5) // check number of nickles to cents.
         {
             cents -= 5;
             count++;
         }
         else
         {
-            cents -= 1;
+            cents -= 1; // check number of pennies to cents.
             count++;
         }
     }
