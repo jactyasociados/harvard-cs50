08ba746240bfecdd9b87f53cd2ddbf29c08a86ad
Javier Cristobal <jctyasociados@gmail.com>
2022-02-25 23:43:09 +0100

    cash

--- cash/cash.c
+++ cash/cash.c
@@ -1,81 +1,55 @@
-#include <cs50.h>
+#include <cs50.h>  /* include cs50.h  Header file */
 #include <stdio.h>
-#include <math.h>
- 
-//Define Variables
- 
-int cents;
-int quarters;
-int dimes;
-int nickels;
-int pennies;
+#include <math.h>  /* include math.h Header file */
 
-// Prototype functions
-
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
-int calculate_pennies(int cents);
- 
-int main(void)
+int main(int argc, char *argv[]) /* the river Code */
 {
-    float change;
+    /*cointCounter is Variable to hold the number of coin
+     will be used as counter initialize to zero for now*/
+    int cointCounter = 0;
+
+    int cents; // cents variable
+
+    float userIput; // Variable to hold user input
+
     do
     {
-        change = get_float("Change owed: ");
-        cents_owed = round(change * 100);
+        // Ask user to input  the amount of dollars to chance
+
+        userIput = get_float("Change owed  :");
+
+        // converts user input money from dollars to cents
+        cents = round(userIput * 100);
     }
-    while (cents_owed <= 0);
-    
-    
- 
-    //Calclate Quarters
-    int calculate_quarters(int cents);
+    //  telling user to in input only non-negative value.
+    while (userIput < 0);
+
+    /* handle corner cases */
+
+    while (cents >= 25) // to count quarters change
     {
-        while(cents>=25)
-        {
-            quarters++;
-            cents-=25;
-        }
+        cents = cents - 25;
+        cointCounter++; // increment cointCounter by one
     }
- 
-    //Calculate Dimes
-    int calculate_dimes(int cents);
+
+    while (cents >= 10) // to count dimes  change
     {
-        while(cents>=10)
-        {
-            dimes++;
-            cents-=10;
-        }
+        cents = cents - 10;
+        cointCounter++; // increment cointCounter by one
     }
- 
-    //Calculate Nickels
-    int calculate_nickels(int cents);
+    while (cents >= 5)  // to count nickels change
     {
-        while(cents>=5)
-        {
-            nickels++;
-            cents-=5;
-        }
+        cents = cents - 5;
+        cointCounter++; // increment cointCounter by one
     }
- 
-    //Calculate Pennies
-    int calculate_pennies(int cents);
+    while (cents >= 1) // to count pennies change
     {
-        while(cents>=1)
-        {
-            pennies++;
-            cents-=1;
-        }
+        cents = cents - 1;
+        cointCounter++; // increment cointCounter by one
     }
-    
-    calculate_quarters(cents_owed);
-    calculate_dimes(cents_owed);
-    calculate_nickels(cents_owed);
-    calculate_pennies(cents_owed);
-    //Total Coins
-    int coins=quarters+dimes+nickels+pennies;
-    printf("You have %d coins. \n",coins);
- 
- 
-}
+
+    printf("%i \n", cointCounter); // print the result
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */
