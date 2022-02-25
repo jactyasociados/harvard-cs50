014ba878f4d5c6be6b14eedaeca2b8acd9a58372
Javier Cristobal <jctyasociados@gmail.com>
2022-02-25 22:45:10 +0100

    cash

--- cash/cash.c
+++ cash/cash.c
@@ -1,72 +1,54 @@
 #include <stdio.h>
-#include <cs50.h>
 #include <math.h>
+#include <cs50.h>
 
-/* Ask user for change owed as floating point, and return change as number of
- * cents (an int), rounded correctly. Re-prompts if non-number or number less 
- * than 0 given.
- */
-int ask_change(void)
-{
-    float change;
-    
-    printf("O hai! ");
-    do
-    {
-        printf("How much change is owed?\n");
-        change = GetFloat();
-    }
-    while (change < 0);
-    
-    int cents = (int) round(change * 100);
-    
-    return cents;
-}
-
-/* Take number of cents required as change, and return an integer giving the
- * minimum number of coins that can be given in change. Assumes that the only 
- * coins available are quarters (25¢), dimes (10¢), nickels (5¢),
- * and pennies (1¢).
- */
-int min_coins(int cents)
-{
-    const int quarter = 25;
-    const int dime = 10;
-    const int nickel = 5;
-    const int penny = 1;
-    
-    int coins = 0;
-    
-    while (cents > 0)
-    {
-        /* Determine the largest type of coin you can give, and deduct this from
-           amount of change remaining to return */
-        if (cents >= quarter)
-        {
-            cents -= quarter;
-        }
-        else if (cents >= dime)
-        {
-            cents -= dime;
-        }
-        else if (cents >= nickel)
-        {
-            cents -= nickel;
-        }
-        else if (cents >= penny)
-        {
-            cents -= penny;
-        }
-        
-        // increment the coin counter
-        coins++;
-    }
-    
-    return coins;
-}
+float getValue(void);
+int coins(int cents);
 
 int main(void)
 {
-    int coins = min_coins(ask_change());
-    printf("Coins owed: %i\n", coins);
+    float owed = getValue();
+    int cents = round(owed * 100);
+    printf("Coins returned: %d\n", coins(cents));
+}
+
+float getValue(void)
+{
+    float change;
+    do
+    {
+        change = get_float("Change owed: ");
+    }
+    while (change < 0.00);
+return change;
+}
+
+int coins(int cents)
+{
+    int count = 0;
+    
+    while (cents > 0)
+    {
+        if (cents >= 25)
+        {
+            cents -= 25;
+            count++;
+        }
+        else if (cents >= 10)
+        {
+            cents -= 10;
+            count++;
+        }
+        else if (cents >= 5)
+        {
+            cents -= 5;
+            count++;
+        }
+        else
+        {
+            cents -= 1;
+            count++;
+        }
+    }
+    return count;
 }
