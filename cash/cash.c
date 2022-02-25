fbb597d43205e7bb069d782c6568e9af5cd1d0a3
Javier Cristobal <jctyasociados@gmail.com>
2022-02-25 23:19:09 +0100

    cash

--- cash/cash.c
+++ cash/cash.c
@@ -1,57 +1,81 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <math.h>
-#include <cs50.h>
+ 
+//Define Variables
+ 
+int cents;
+int quarters;
+int dimes;
+int nickels;
+int pennies;
 
-// Function to get the change owed in a float value.
-float getValue(void);
-// Function that counts the cents coins as an integer.
-int coins(int cents);
+// Prototype functions
 
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
+ 
 int main(void)
-{
-    float owed = getValue();
-    int cents = round(owed * 100);
-    // print number of cent coins returned
-    printf("Coins returned: %d\n", coins(cents));
-}
-
-float getValue(void)
 {
     float change;
     do
     {
         change = get_float("Change owed: ");
+        cents_owed = round(change * 100);
     }
-    while (change < 0.00);
-    return change;
-}
-
-int coins(int cents)
-{
-    int count = 0;
+    while (cents_owed <= 0);
     
-    while (cents > 0)
+    
+ 
+    //Calclate Quarters
+    int calculate_quarters(int cents);
     {
-        if (cents >= 25) // check number of quarters to cents.
+        while(cents>=25)
         {
-            cents -= 25;
-            count++;
-        }
-        else if (cents >= 10) // check number of dimes to cents.
-        {
-            cents -= 10;
-            count++;
-        }
-        else if (cents >= 5) // check number of nickles to cents.
-        {
-            cents -= 5;
-            count++;
-        }
-        else
-        {
-            cents -= 1; // check number of pennies to cents.
-            count++;
+            quarters++;
+            cents-=25;
         }
     }
-    return count;
+ 
+    //Calculate Dimes
+    int calculate_dimes(int cents);
+    {
+        while(cents>=10)
+        {
+            dimes++;
+            cents-=10;
+        }
+    }
+ 
+    //Calculate Nickels
+    int calculate_nickels(int cents);
+    {
+        while(cents>=5)
+        {
+            nickels++;
+            cents-=5;
+        }
+    }
+ 
+    //Calculate Pennies
+    int calculate_pennies(int cents);
+    {
+        while(cents>=1)
+        {
+            pennies++;
+            cents-=1;
+        }
+    }
+    
+    calculate_quarters(cents_owed);
+    calculate_dimes(cents_owed);
+    calculate_nickels(cents_owed);
+    calculate_pennies(cents_owed);
+    //Total Coins
+    int coins=quarters+dimes+nickels+pennies;
+    printf("You have %d coins. \n",coins);
+ 
+ 
 }
