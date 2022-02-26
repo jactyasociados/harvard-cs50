a8e06520c6b8f6b511b283ad17d7d31cacec55b8
Javier Cristobal <jctyasociados@gmail.com>
2022-02-26 12:40:18 +0100

    cash

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -40,7 +40,7 @@ int get_cents(void)
     int cents;
     do
     {
-      cents = get_int("How many cents are owed: ");
+        cents = get_int("How many cents are owed: ");
     }
     while (cents < 0);
     
@@ -54,8 +54,8 @@ int calculate_quarters(int cents)
     int quarters = 0;
     while (cents >= 25) 
     {
-      cents = cents - 25;
-      quarters++;
+        cents = cents - 25;
+        quarters++;
     }
     return quarters;
 }
@@ -66,8 +66,8 @@ int calculate_dimes(int cents)
     int dimes = 0;
     while (cents >= 10)
     {
-      cents = cents - 10;
-      dimes++;
+        cents = cents - 10;
+        dimes++;
     }
     return dimes;
 }
@@ -78,8 +78,8 @@ int calculate_nickels(int cents)
     int nickels = 0;
     while (cents >= 5)
     {
-      cents = cents - 5;
-      nickels++;
+        cents = cents - 5;
+        nickels++;
     }
     return nickels;
 }
@@ -90,8 +90,8 @@ int calculate_pennies(int cents)
     int pennies = 0;
     while (cents >= 1)
     {
-      cents = cents - 1;
-      pennies++;
+        cents = cents - 1;
+        pennies++;
     }
     return pennies;
 }
